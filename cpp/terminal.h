43d5f0c35a37faade7ee1ce26234e0e67afd84bb
emilka <emilfp2020@gmail.com>
2025-12-23 21:46:24 +0300

    dopolnil terminal.h

--- cpp/terminal.h
+++ cpp/terminal.h
@@ -2,7 +2,17 @@
 #define TERMINAL_H
 
 #include "bmp.h"
+#include <vector>
+#include <cstdint>
+#include <cstdio>
 
+struct Image {
+    int width;
+    int height;
+    std::vector<uint8_t> pixels;
+};
+
+bool getTerminalSize(int& cols, int& rows);
 void renderToTerminal(const std::vector<pixel> &img);
 void getPixel(const Image& img, int x, int y, uint8_t& r, uint8_t& g, uint8_t& b);
 #endif
